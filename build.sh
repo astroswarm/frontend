ef46664eec451d9df1d5df4d57484296fc98a82a
Robby Grossman <robby@freerobby.com>
2017-06-10 12:58:16 -0400

    Begin fork of private client for public interface; prep for buildpack-based deploy.

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-elm-make Main.elm --output main.js --warn --yes
-
-cp index.html build/
-cp main.js build/
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-elm-live --debug Main.elm --output=main.js --open
