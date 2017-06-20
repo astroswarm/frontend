4797b25180de754686f24922b43b006263635115
Robby Grossman <robby@freerobby.com>
2017-06-20 05:15:40 -0400

    Update copy to more clearly reflect our master plan.

--- src/ViewAbout.elm
+++ src/ViewAbout.elm
@@ -14,6 +14,7 @@ view =
             , Html.a [ Html.Attributes.href "mailto:robby@freerobby.com" ] [ Html.text "Email Robby" ]
             , Html.text " for details."
             ]
-        , Html.p [] [ Html.text "After we build a community, we'll have a global network of telescopes. We'll use this network to make it easy to collaborate with other astronomers, and to share your findings with the larger world. People who don't have or can't afford telescopes will be able to use and control them for free over the internet." ]
-        , Html.p [] [ Html.text "Over time, our platform will model how humans observe space, and then allow the telescopes to look for things entirely on their own. Because they're networked, they can coordinate with each other for around-the-clock coverage of interesting celestial phenomena, and we'll learn more about space than we ever have before." ]
+        , Html.p [] [ Html.text "After we build a community, we'll have a global network of telescopes. We'll use this network to make it easy to collaborate with other astronomers, and to share your findings with the larger world. People who don't have telescopes will be able to use and control them over the internet, and people who can't afford them will be able to participate for free." ]
+        , Html.p [] [ Html.text "Over time, our platform will model how humans observe space, and allow the telescopes to look for things entirely on their own. Because they're networked, they can coordinate to make optimal use of hardware and geographic dispersion, both in search of new discoveries, and to provide around-the-clock coverage of known celestial events and phenomena." ]
+        , Html.p [] [ Html.text "We hope you'll join us to make space study more accessible to everyone, and to learn more about our cosmos than we ever have before." ]
         ]
