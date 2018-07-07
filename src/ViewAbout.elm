2ebcfd9f19a472ddedde6469587643de81c6ef6a
Robby Grossman <robby@freerobby.com>
2018-07-07 12:45:13 -0400

    Reduce copy on main page.

--- src/ViewAbout.elm
+++ src/ViewAbout.elm
@@ -8,12 +8,13 @@ view : Html.Html msg
 view =
     Html.div []
         [ Html.h1 [] [ Html.text "Welcome to Your Astrolab" ]
-        , Html.p [] [ Html.text "Right now you can only communicate with your Astrolab over a Local Area Network. Internet-based control is coming soon, but I have been too busy with feature development to tend to the security concerns that internet-based controls require. I will launch internet-based control as soon as I can do so responsibly; I'm aiming for early June." ]
-        , Html.p [] [ Html.text "The services running on your device are listed in the dropdown menu above. Select any service to control it via a web-based VNC session. All services remain running in the background when you are not viewing them." ]
+        , Html.p [] [ Html.text "The applications running on your Astrolab are listed in the menu above. Select any application to begin using it. Applications run in the background whenever your device is on, so it's safe to close your browser tab." ]
+        , Html.p [] [ Html.text "Astrolab runs on a Local Area Network. Internet-based control is coming to public beta later this year." ]
+        , Html.h5 [] [ Html.text "Need Help?" ]
         , Html.p []
-            [ Html.text "Do you need more help? Please "
-            , Html.a [ Html.Attributes.href "mailto:robby@freerobby.com" ] [ Html.text "email me" ]
-            , Html.text " with any questions you have or difficulties you run into."
+            [ Html.text "Please email me at "
+            , Html.a [ Html.Attributes.href "mailto:robby@freerobby.com" ] [ Html.text "robby@freerobby.com" ]
+            , Html.text " with any questions or difficulties."
             ]
-        , Html.p [] [ Html.text "In order to make your problems easier to debug, I've built a system to automatically upload all the logs from your Astrolab. Please find this tool in the dropdown menu above, and include a link to your logs in your email when seeking help." ]
+        , Html.p [] [ Html.text "To make problems easier to debug, your Astrolab includes a tool to upload all of its logs for analysis. Please find this tool in the dropdown menu above, and include a link to your logs in your email when seeking help." ]
         ]
